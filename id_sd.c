a0cb8ab2a92a6afc7e8e5532c58b255ce9b0791d
KillerSoftBP <emelya177@gmail.com>
2024-08-02 20:17:42 +0100

    Improve sound attenuation
    This isn't a perfect fix, since the values passed to SD_SetPosition simply aren't suitable for SDL_Mixer's volume. The sounds tend to pop in loudly as you get closer to the source, instead of a smooth transition in volume.

--- id_sd.c
+++ id_sd.c
@@ -378,8 +378,7 @@ void SD_SetPosition(int channel, int leftpos, int rightpos)
     {
         case sds_SoundBlaster:
 //            SDL_PositionSBP(leftpos,rightpos);
-            Mix_SetPanning(channel, ((15 - leftpos) << 4) + 15,
-                ((15 - rightpos) << 4) + 15);
+            Mix_SetPanning(channel,255 - (leftpos * 28),255 - (rightpos * 28));
             break;
 
         default:
